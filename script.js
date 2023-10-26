3222fab9306e346fe2004c6b97c32f3f44201afe
guptaparth9114 <117535633+guptaparth9114@users.noreply.github.com>
2023-10-27 01:21:25 +0530

    Final

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-let back = document.querySelector(".back");
 let chatBox = document.querySelector(".chatBox");
-let open = document.querySelector(".open");
+let prathamChatBox = document.getElementById("pratham");
+let prathamContact = document.querySelector(".listHead h4");
+let backButton = document.querySelector(".back");
+let chatsTab = document.getElementById("chats");
 
-back.onclick = function () {
-  chatBox.classList.add("hide");
-};
+// Initially hide the chatbox for Pratham Jain
+prathamChatBox.style.display = "none";
 
-open.onclick = function () {
-  chatBox.classList.remove("hide");
-};
+// Click event for the Pratham Jain contact
+prathamContact.addEventListener("click", function () {
+  // Hide the main chat section
+  chatBox.style.display = "none";
+
+  // Show the chatbox for Pratham Jain
+  prathamChatBox.style.display = "block";
+});
+
+// Click event for the "Back" button in the chatbox
+backButton.addEventListener("click", function () {
+  // Hide the chatbox for Pratham Jain
+  prathamChatBox.style.display = "none";
+
+  // Show the main chat section
+  chatBox.style.display = "block";
+});
+
+// Click event for the "Chats" tab to go back to the main chat contacts display
+chatsTab.addEventListener("click", function () {
+  // Show the main chat section
+  chatBox.style.display = "block";
+
+  // Hide the chatbox for Pratham Jain
+  prathamChatBox.style.display = "none";
+});
